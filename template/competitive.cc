c9fcd3da3cbb689ddf8d4b7cfa64873e50ed4292
Kakudo <kakudo415@gmail.com>
2020-05-03 14:33:11 +0900

    -DEBUG macro, rep macro

--- template/competitive.cc
+++ template/competitive.cc
@@ -7,4 +7,12 @@ using namespace std;
 using ll = long long;
 using ld = long double;
 
+#ifdef EBUG
+#define debug(x) cout << "\033[31m" << #x << ": " << x << "\033[0m\n";
+#else
+#define debug(x)
+#endif
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
 int main() {}
